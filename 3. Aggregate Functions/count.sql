d8f796720aa25b4d3291019606590a8bea3f309c
Sharvil Patil <sharvilpatil13@gmail.com>
2022-09-20 20:55:46 +0530

    sum()

---
+++ 3. Aggregate Functions/count.sql
@@ -0,0 +1,6 @@
+use sql_practice;
+SELECT * FROM Result;
+
+/* COUNT(): Counts values*/
+
+SELECT COUNT(Marks) FROM Result WHERE Marks>=45;
---
+++ 3. Aggregate Functions/sum.sql
@@ -0,0 +1,6 @@
+use sql_practice;
+SELECT * FROM Result;
+
+/* SUM(): Calculates sum */
+
+SELECT SUM(Marks) FROM Result;
